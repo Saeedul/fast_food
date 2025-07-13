0098a50dcb7a24bd56e9f31eae6a2643aa9fa564
Saeedul <karimsaeedul476@gmail.com>
2025-07-14 01:23:03 +0600

    added items in home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,45 @@
-import { Text, View } from "react-native";
+import { images, offers } from "@/constants";
+import cn from "clsx";
+import { Fragment } from "react";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl text-center text-blue-500 font-quicksand-bold">
-        Welcome to Nativewind!
-      </Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+
+          return (
+            <View>
+              <Pressable
+                className={cn("offer-card", isEven ? "flex-row-reverse" : "flex-row")}
+                style={{ backgroundColor: item.color }}>
+                {({ pressed }) => (
+                  <Fragment>
+                    <View className={"h-full w-1/2"}>
+                      <Image source={item.image} className={"size-full"} resizeMode={"contain"} />
+                    </View>
+
+                    <View className="offer-card__info">
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image 
+                      source={images.arrowRight}
+                      className="size-10"
+                      resizeMode="contain"
+                      tintColor="#ffffff"
+                      />
+                    </View>
+                  </Fragment>
+                )}
+              </Pressable>
+            </View>
+          )
+        }} />
+    </SafeAreaView>
   );
 }
