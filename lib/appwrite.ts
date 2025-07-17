594b6aee642f5a662a746524ecb0d8b8c685644c
Saeedul <karimsaeedul476@gmail.com>
2025-07-17 18:08:41 +0600

    setup appwrite and configured

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.karim.fastfood",
+    projectID: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+}
